f7378bd140971d5d2baabe6e7d5d6e96e3e016f4
Kreeblah <kreeblah@gmail.com>
2021-03-28 02:39:44 -0700

    Initial commit

---
+++ dic-cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+find . -name '*.bin' -type f -delete
+find . -name '*.c2' -type f -delete
+find . -name '*.dat' -type f -delete
+find . -name '*.gz' -type f -delete
+find . -name '*.txt' -type f -delete
+find . -name '*_img.cue' -type f -delete
+
+find . -name '*.cue' -type f -print0 | xargs -0 sed -i '' -e 's/\.bin/\.img/g'
